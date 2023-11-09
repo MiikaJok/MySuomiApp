1f12c1247395f9b96ac201c733d3c3764d7990e6
Miika <miikajok@metropolia.fi>
2023-11-09 15:49:17 +0200

    mitä kuuluu?

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MySuomiApp
+//
+//  Created by iosdev on 9.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world is on fire :)))))))")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
