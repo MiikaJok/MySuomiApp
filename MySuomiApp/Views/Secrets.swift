f9030d09bc9089707cc52a64fcfcab985b793563
MiikaJok <104007329+MiikaJok@users.noreply.github.com>
2023-11-14 17:42:05 +0200

    Update Secrets.swift

--- MySuomiApp/Views/Secrets.swift
+++ MySuomiApp/Views/Secrets.swift
@@ -1,12 +1,1 @@
-//
-//  Secrets.swift
-//  MySuomiApp
-//
-//  Created by iosdev on 14.11.2023.
-//
 
-import Foundation
-
-struct Secrets {
-    static let googleMapsApiKey = "AIzaSyDh8Fsdpg3FDH7Jbkq1RsI9BCCqQrQH9gQ"
-}
