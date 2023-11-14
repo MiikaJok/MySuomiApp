f2730438328917eefaef31d4df1623eb46e76f07
ellasig <103430174+ellasig@users.noreply.github.com>
2023-11-14 11:41:49 +0200

    -

--- MySuomiApp/ContentView.swift
+++ MySuomiApp/ContentView.swift
@@ -1,21 +1,41 @@
-//
-//  ContentView.swift
-//  MySuomiApp
-//
-//  Created by iosdev on 9.11.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isMenuVisible = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world! Mitä kuuluu?????")
+        NavigationView {
+            VStack {
+                // Button to toggle the menu
+                Button(action: {
+                    withAnimation {
+                        self.isMenuVisible.toggle()
+                    }
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .imageScale(.large)
+                        .padding()
+                }
+                .padding(.top, 20)
+                
+                Spacer()
+                .hoverEffect()
+
+                if isMenuVisible {
+                    // Your navigation menu content
+                    List {
+                        Text("Eat and drink")
+                        Text("Sights")
+                        Text("fun")
+                    }
+                    .frame(width: 200)
+                    .background(Color.gray)
+                }
+                Spacer()
+            }
+            .navigationTitle("SwiftUI Navigation Menu")
+            .navigationBarHidden(true) // Hide the default navigation bar
         }
-        .padding()
     }
 }
 
