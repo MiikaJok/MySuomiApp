09d949ee91697b7ae527185e59ef0c1d654f177f
darjapo <darjapo@metropolia.fi>
2023-11-15 18:47:15 +0200

    tärkee push

--- MySuomiApp/Views/MapView.swift
+++ MySuomiApp/Views/MapView.swift
@@ -8,13 +8,13 @@ import MapKit
 
 struct MapView: View {
     private let manager = CLLocationManager()
-
+    
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 60.1695, longitude: 24.9354), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-
+    
     @EnvironmentObject var languageSettings: LanguageSettings //
     
     @State private var searchText = ""
-
+    
     var body: some View {
         VStack {
             HStack {
@@ -29,7 +29,7 @@ struct MapView: View {
                         .cornerRadius(8)
                         .padding(8)
                 }
-
+                
                 Text("MySuomiApp")
                     .padding(8)
                     .font(.title)
@@ -38,41 +38,41 @@ struct MapView: View {
             }
             
             // Zoom In Button
-                        Button(action: {
-                            self.region.span.latitudeDelta /= 2
-                            self.region.span.longitudeDelta /= 2
-                        }) {
-                            Text("Zoom In")
-                                .padding(8)
-                                .foregroundColor(.white)
-                                .background(Color.green)
-                                .cornerRadius(8)
-                                .padding(8)
-                        }
-
-                        // Zoom Out Button
-                        Button(action: {
-                            self.region.span.latitudeDelta *= 2
-                            self.region.span.longitudeDelta *= 2
-                        }) {
-                            Text("Zoom Out")
-                                .padding(8)
-                                .foregroundColor(.white)
-                                .background(Color.red)
-                                .cornerRadius(8)
-                                .padding(8)
-                        }
-
+            Button(action: {
+                self.region.span.latitudeDelta /= 2
+                self.region.span.longitudeDelta /= 2
+            }) {
+                Text("Zoom In")
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(8)
+                    .padding(8)
+            }
+            
+            // Zoom Out Button
+            Button(action: {
+                self.region.span.latitudeDelta *= 2
+                self.region.span.longitudeDelta *= 2
+            }) {
+                Text("Zoom Out")
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(8)
+                    .padding(8)
+            }
+            
             TextField(languageSettings.isEnglish ? "Search" : "Haku", text: $searchText)
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-
-           
+            
+            
             // Map View
             Map(coordinateRegion: $region)
                 .frame(width: 400, height: 300)
-
+            
             Spacer()
         }
         .environment(\.locale, languageSettings.isEnglish ? Locale(identifier: "en") : Locale(identifier: "fi"))
